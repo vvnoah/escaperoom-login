ff7068834e414720799034856e14f2067de3fcf9
Noah Van Vaerenbergh <vvnoah@hotmail.com>
2022-04-24 00:56:50 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const inputs = document.querySelectorAll("input");
+const form = document.querySelector("form");
+const correct = document.getElementById("correct");
+const wrong = document.getElementById("wrong");
+
+inputs.forEach((input, key)=>{
+    if (key !== 0) {
+        input.addEventListener("click", function(){
+            inputs[0].focus();
+        });
+    }
+
+    input.addEventListener('keyup', function () {
+        if (input.value) {
+          if (key === 5) {
+            userCode = [...inputs].map((input) => input.value).join('');
+          } else {
+            inputs[key + 1].focus();
+          }
+        }
+      });
+});
+
+function validate(){
+    const pincode = 123456;
+    //alert(userCode);
+    if (userCode == pincode) {
+      //alert("pincode is correct");
+      event.preventDefault();
+      window.location = "solution.html";
+      
+    } else {
+      
+      //alert("pincode wrong");
+    }
+}
